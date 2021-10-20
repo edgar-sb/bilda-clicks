874470f6286105844ecdc7e996a33d90505ecc2e
fer <fernandovg@pharospayments.com>
2021-10-20 16:18:54 -0500

    okey

--- Bilda/index.js
+++ Bilda/index.js
@@ -1,36 +1,107 @@
 var axios = require('axios')
 
 class Bilda {
-    token = null
+  site_id = null
 
-    constructor(token){
-        this.token = token
-    }
+  constructor(site_id){
+    this.site_id = site_id
+  }
 
-    async createCollection(name, fields){
-        var data = JSON.stringify({
-            "name": name,
-            "customer_lock": "unlocked",
-            "fields": fields,
-            "external_details": {
-              "enabled": "false"
-            }
-          });
+  async createCollection(name, fields) {
+    var data = {
+      "name": name,
+      "customer_lock": "unlocked",
+      "fields": fields,
+      "external_details": {
+        "enabled": "false"
+      }
+    };
+
+    var config = {
+      method: 'post',
+      url: 'https://api.duda.co/api/sites/multiscreen/14557a2c/collection',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': "Basic ZDIzOTYwYmY1MTpaRnlqQTVzVzdYSW8="
+      },
+      data: data
+    };
+
+    let response = await axios(config).then(res => { return res }).catch(e => { return e });
+    return response;
+  }
+
+
+  async sumActionU(find) {
+    this.getNumber(this.site_id).then(res=> {
+      this.findField(res.data.values, find)
+    })
+  }
+
+  async plus(id){
+    var data = JSON.stringify([
+      {
+        "id": id,
+        "data": {
           
-          var config = {
-            method: 'post',
-            url: 'https://api.duda.co/api/sites/multiscreen/14557a2c/collection',
-            headers: { 
-              'Accept': 'application/json', 
-              'Content-Type': 'application/json', 
-              'Authorization': this.token
-            },
-            data : data
-          };
-          
-         return await axios(config).then( res => {return res.data}).catch(e => {return e});
+        }
+      }
+    ]);
+    
+    var config = {
+      method: 'put',
+      url: 'https://api.duda.co/api/sites/multiscreen/14557a2c/collection/publishing/row',
+      headers: { 
+        'Accept': 'application/json', 
+        'Content-Type': 'application/json', 
+        'Authorization': 'Basic ZDIzOTYwYmY1MTpaRnlqQTVzVzdYSW8='
+      },
+      data : data
+    };
+    
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    
+  }
 
+  async getNumber(site_id) {
+    var config = {
+      method: 'get',
+      url: `https://api.duda.co/api/sites/multiscreen/${site_id}/collection/clicks`,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic ZDIzOTYwYmY1MTpaRnlqQTVzVzdYSW8='
+      }
+    };
+
+    let clicks = await axios(config)
+      .then(function (response) {
+        return response
+      })
+      .catch(function (error) {
+        return error
+      });
+
+      return clicks
+  }
+
+  findField(arr, search){
+    if(arr !== null && arr !== undefined){
+      arr.map(i => {
+        if (i.id === search){
+          let clicks = i.data.click
+          this.plus()
+        }
+      });
     }
+  }
 }
 
-module.exports =  Bilda;
+module.exports = Bilda;
--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,36 @@ var Bilda = require('../Bilda')
 
 /* GET home page. */
 router.post('/create_collection', function(req, res, next) {
-  let token = req.body.token;
-  let bilda = new Bilda(token);
+  let bilda = new Bilda();
 
   var name = req.body.name
   var fields = req.body.fields
   var reponseOfBilda = bilda.createCollection(name, fields);
   reponseOfBilda.then(r => {
-    res.json({message: r})
+    console.log(r)
+    if(r.status === 204){
+      res.json({message: "Creado"})  
+    } else{
+      console.log(r.response.status)
+      res.json({message: "Revisa tu configuracion"})
+    }
   })
   
+});
+
+router.post('/clicked', (req, res, next)=>{
+    let site_id = req.body.id
+    let field_id = req.body.field
+    let bilda = new Bilda(site_id);
+    
+    // bilda.getNumber(site_id).then(r => {
+    //   if(r.status === 200){
+    //     res.json({message: r.data.values})  
+    //   }
+
+    // });
+    bilda.sumActionU(field_id);
+    
 });
 
 module.exports = router;
